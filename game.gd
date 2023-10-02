ffef1cac629135d045fe4a9293e76fa3fa117269
DanielCandido <danielfelipec18@hotmail.com>
2023-10-02 01:32:54 -0300

    [FEAT] Adding castle in scene

--- game.gd
+++ game.gd
@@ -5,6 +5,8 @@ extends Node2D
 var _inventory_is_opened = false
 var _shop_is_opened = false
 
+var troop
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player._follow_camera(camera)
@@ -17,6 +19,9 @@ func _process(delta):
 	var _player_process = !_inventory_is_opened and !_shop_is_opened
 	player.set_physics_process(_player_process)
 	
+	if troop != null and Input.is_action_just_pressed("interaction"):
+		print("load scene")
+	
 func _open_shop():
 	player._cancel_move()
 	await get_tree().create_timer(1).timeout
@@ -26,3 +31,15 @@ func _open_shop():
 func _close_shop():
 	player._return_move()
 	_shop_is_opened = false
+
+
+func _on_castle_door_body_entered(body: CharacterBody2D):
+	if body.is_in_group("player"):
+		troop = body
+		$CastleDoor/Label.visible = true
+
+
+func _on_castle_door_body_exited(body):
+	if body.is_in_group("player"):
+		troop = null
+		$CastleDoor/Label.visible = false
